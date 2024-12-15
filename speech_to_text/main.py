722ab72ddd56766b9dfde72f474f57186777ad22
jeff.vincent <jeff.d.vincent@gmail.com>
2024-12-15 15:55:07 -0800

    WIP -- translates one chunk / mp3

--- speech_to_text/main.py
+++ speech_to_text/main.py
@@ -4,20 +4,17 @@ import os
 import redis
 import base64
 import websockets
-
-async def connect_socket(uri):
-    async with websockets.connect(uri) as websocket:
-        return websocket
+import assemblyai
+import random
+import string
 
 class SpeechToText():
     def __init__(self):
-        # self.vosk_uri = os.environ.get('VOSK_URI')
-        self.vosk_uri = 'ws://localhost:2700'
         self.redis_host = os.environ.get('REDIS_HOST')
         self.redis_port = os.environ.get('REDIS_PORT')
         # self.r = redis.Redis(host=self.redis_host, port=self.redis_port, db=0)
         self.r = redis.Redis()
-        self.vosk_ws_connection = None
+        self.assembly_ai = assemblyai.settings.api_key = '' 
         
     async def connect_socket(self):
         async with websockets.connect(self.vosk_uri) as websocket:
@@ -32,25 +29,53 @@ class SpeechToText():
             data = json.loads(data)
             # convert json back to original bytes
             audio_chunk = base64.b64decode(data['audio_chunk'])
-            return audio_chunk
+            # write to tmp file; return filepath
+            random_string = ''.join(random.choices(string.ascii_letters,k=7))
+            filepath = f'{random_string}.mp3'
+            with open(filepath, 'wb') as f:
+                f.write(audio_chunk)
+            return filepath
 
-    async def convert_audio_to_text(self, data):
-        # TODO: replace vosk with something else...
+    async def convert_audio_to_text(self, filepath):
+        # Assembly AI https://www.assemblyai.com/docs/api-reference/streaming/realtime#handshake
+        transcriber = assemblyai.Transcriber()
+        transcript = transcriber.transcribe(filepath)
+
+
+        if transcript.status == assemblyai.TranscriptStatus.error:
+            print(transcript.error)
+        else:
+            print(transcript.text)
+            return transcript.text
+
         return None
+    
+    # def on_data(self, transcript: assemblyai.RealtimeTranscript):
+    #     print(transcript)
+    #     print(type(transcript))
+    #     # if not transcript.text:
+    #     #     return
+
+    #     # if isinstance(transcript, assemblyai.RealtimeFinalTranscript):
+    #     #     print(transcript.text, end="\r\n")
+    #     # else:
+    #     #     print(transcript.text, end="\r")
+
+    # def on_error(self, error: assemblyai.RealtimeError):
+    #     print("An error occured:", error)
 
     async def write_text_to_redis_queue(self, text):
-        # build dict with text
-        # convert dict to json
-        # write json to redis
-        pass
+        self.r.rpush('written_text', text)
+        return None
 
     async def run(self):
         while True:
-            audio_chunk = await self.read_audio_from_redis_queue()
-            if audio_chunk:
-                await self.connect_socket()
-                text = await self.convert_audio_to_text(audio_chunk)
-                await self.write_text_to_redis_queue(text)
+            filepath = await self.read_audio_from_redis_queue()
+            if filepath:
+                # await self.connect_socket()
+                text = await self.convert_audio_to_text(filepath)
+                if text:
+                    await self.write_text_to_redis_queue(text)
 
 if __name__ == "__main__":
     speech_to_text = SpeechToText()
--- text_translation/main.py
+++ text_translation/main.py
@@ -1,31 +1,51 @@
 import asyncio
 import os
 import redis
+import json
+from openai import OpenAI
 
 class TextTranslator():
     def __init__(self):
         self.redis_host = os.environ.get('REDIS_HOST')
         self.redis_port = os.environ.get('REDIS_PORT')
-        self.r = redis.Redis(host=self.redis_host, port=self.redis_port, db=0)
+        self.r = redis.Redis()
         self.incoming_text = None
         self.output_text = None
+        self.openai_api_client = OpenAI(api_key='')
 
 
     async def read_text_from_redis_queue(self):
-        pass
+        key, data = self.r.blpop("written_text", timeout=0)
+        if data:
+            print(f"Processing data from {key}")
+            print(data)
+            return data
 
-    async def translate_text(self):
-        pass
+    def translate_text(self, data):
+        prompt = f"If the following text is in English, translate it to Spanish, and if it is in Spanish, translate it to English: {data}"
+        response = self.openai_api_client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                { 'role': "system", 'content': "You are a helpful assistant." },
+                {
+                    'role': "user",
+                    'content': prompt,
+                },
+            ],
+        )
+        return response.choices[0].message.content
+        
 
     async def write_translated_text_to_redis_queue(self):
         pass
 
     async def run(self):
         while True:
-            self.incoming_text = self.read_text_from_redis_queue()
-            if self.incoming_text:
-                translated_text = self.translate_text(self.incoming_text)
-                self.write_translated_text_to_redis_queue(translated_text)
+            data = await self.read_text_from_redis_queue()
+            if data:
+                translated_text = self.translate_text(data)
+                print(translated_text)
+            #     self.write_translated_text_to_redis_queue(translated_text)
 
 if __name__ == "__main__":
     text_translator = TextTranslator()
